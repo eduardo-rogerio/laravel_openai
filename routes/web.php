cad7a6ce3ac52e1d110e10d7299472eb957ead6d
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2024-03-13 09:38:18 -0300

    Revert "feat(): aula 8"
    This reverts commit 7e0c092803a785560d04f1a3f6490bf22da01ae5.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\AI\Assistant;
 use Illuminate\Support\Facades\Route;
-use OpenAI\Laravel\Facades\OpenAI;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,105 +15,54 @@ use OpenAI\Laravel\Facades\OpenAI;
 |
 */
 
-//Route::get('/', function () {
-//    $chat = new Assistant();
-//    $chat->systemMessage('You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.')
-//        ->send('Compose a poem that explains the concept of recursion in programming.');
-//    $sillyPoem = $chat->reply('Cool, can you make it much, much sillier.');
-//
-//    return view('welcome', ['response' => $sillyPoem]);
-//});
-//
-//Route::get('/home', function () {
-//    return view('roast');
-//});
-//
-//Route::post('/roast', static function () {
-//    $attributes = request()->validate([
-//        'topic' => ['required', 'string', 'min:3', 'max:50'],
-//    ]);
-//
-//    $prompt = "Please roast {$attributes['topic']} in a sarcastic tone.";
-//
-//    $mp3 = (new Assistant())->send(message: $prompt, speech: true);
-//
-//    $name = md5($mp3);
-//    Storage::disk('local')
-//        ->put('roasts/' . $name . '.mp3', $mp3);
-//
-//    return redirect('/home')->with([
-//        'file' => $name . '.mp3',
-//        'flash' => 'Audio gerado com sucesso!',
-//    ]);
-//});
-//
-//Route::get('/image', function () {
-//    return view('image', [
-//        'messages' => session('messages', []),
-//    ]);
-//});
-//
-//Route::post('/getimage', static function () {
-//    $attributes = request()->validate([
-//        'description' => ['required', 'string', 'min:3'],
-//    ]);
-//
-//    $assistant = new Assistant(session('messages', []));
-//
-//    $assistant->visualize($attributes['description']);
-//
-//    session(['messages' => $assistant->messages()]);
-//
-//    return redirect('/image');
-//});
+Route::get('/', function () {
+    $chat = new Assistant();
+    $chat->systemMessage('You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.')
+        ->send('Compose a poem that explains the concept of recursion in programming.');
+    $sillyPoem = $chat->reply('Cool, can you make it much, much sillier.');
 
-Route::get('/', static function () {
+    return view('welcome', ['response' => $sillyPoem]);
+});
 
-    $file = OpenAI::files()
-        ->upload([
-            'purpose' => 'assistants',
-            'file' => fopen(storage_path('docs/parsing.md'), 'rb'),
-        ]);
+Route::get('/home', function () {
+    return view('roast');
+});
 
-    $assistant = OpenAI::assistants()
-        ->create([
-            'model' => 'gpt-4-1106-preview',
-            'name' => 'Laraparse Tutor',
-            'instructions' => 'You are a helpful programming teacher.',
-            'tools' => [
-                ['type' => 'retrieval'],
-            ],
-            'file_ids' => [
-                $file->id,
-            ],
-        ]);
+Route::post('/roast', static function () {
+    $attributes = request()->validate([
+        'topic' => ['required', 'string', 'min:3', 'max:50'],
+    ]);
 
-    $run = OpenAI::threads()
-        ->createAndRun([
-            'assistant_id' => $assistant->id,
-            'thread' => [
-                'messages' => [
-                    ['role' => 'user', 'content' => 'How do I grab the first paragraph?'],
-                ],
-            ],
-        ]);
+    $prompt = "Please roast {$attributes['topic']} in a sarcastic tone.";
 
-    do {
-        sleep(1);
+    $mp3 = (new Assistant())->send(message: $prompt, speech: true);
 
-        $run = OpenAI::threads()
-            ->runs()
-            ->retrieve(
-                threadId: $run->threadId,
-                runId: $run->id
-            );
+    $name = md5($mp3);
+    Storage::disk('local')
+        ->put('roasts/' . $name . '.mp3', $mp3);
 
-    } while ($run->status !== 'completed');
+    return redirect('/home')->with([
+        'file' => $name . '.mp3',
+        'flash' => 'Audio gerado com sucesso!',
+    ]);
+});
 
-    $messages = OpenAI::threads()
-        ->messages()
-        ->list($run->threadId);
+Route::get('/image', function () {
+    return view('image', [
+        'messages' => session('messages', []),
+    ]);
+});
 
-    dd($messages);
+Route::post('/getimage', static function () {
+    $attributes = request()->validate([
+        'description' => ['required', 'string', 'min:3'],
+    ]);
 
+    $assistant = new Assistant(session('messages', []));
+
+    $assistant->visualize($attributes['description']);
+
+    session(['messages' => $assistant->messages()]);
+
+    return redirect('/image');
 });
